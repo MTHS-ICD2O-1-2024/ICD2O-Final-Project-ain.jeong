7ea53188e731c9fb5c5bc8a50ec1dacc6f6bf0b8
Ain-Jeong <ain.jeong@stu.ocsb.ca>
2025-06-03 00:53:45 +0000

    added 'buying item function'

--- js/inShop.js
+++ js/inShop.js
@@ -7,15 +7,97 @@
 'use strict'
 
 /**
- * This function manages the shop
+ * This function manages all the functions happening in the shop
  */
 // eslint-disable-next-line no-unused-vars
 
-// showing the number of cookies
-window.onload = function () {
-  // input
-  const cookieClick = localStorage.getItem('cookieClick')
+// buy the heart item
+function buyHeartItem () {
+  let cookieClick = Number(localStorage.getItem('cookieClick'))
+  const heartItemPrice = 5
 
-  // output
+  if (cookieClick >= heartItemPrice) {
+    cookieClick -= heartItemPrice
+    localStorage.setItem('cookieClick', cookieClick)
+    localStorage.setItem('boughtHeart', 'bought')
+
+    // save the bought item
+    localStorage.setItem('boughtItem', 'heart-cookie')
+
+    // update the number of cookies left
+    document.getElementById('number-of-cookies').innerHTML = cookieClick + ' Cookies'
+
+    alert('You bought the Heart Cookie!')
+  } else {
+    alert('Not enough cookies to buy this item.')
+  }
+}
+
+// buy the star items
+function buyStarItem () {
+  let cookieClick = Number(localStorage.getItem('cookieClick'))
+  const starItemPrice = 10
+
+  if (cookieClick >= starItemPrice) {
+    cookieClick -= starItemPrice
+    localStorage.setItem('cookieClick', cookieClick)
+    localStorage.setItem('boughtStar', 'bought')
+
+    // save the bought item
+    localStorage.setItem('boughtItem', 'star-cookie')
+
+    // update the number of cookies left
+    document.getElementById('number-of-cookies').innerHTML = cookieClick + ' Cookies'
+
+    alert('You bought the Star Cookie!')
+  } else {
+    alert('Not enough cookies to buy this item.')
+  }
+}
+
+// buy the sunglasses items
+function buySunglassesItem () {
+  let cookieClick = Number(localStorage.getItem('cookieClick'))
+  const sunglassesItemPrice = 15
+
+  if (cookieClick >= sunglassesItemPrice) {
+    cookieClick -= sunglassesItemPrice
+    localStorage.setItem('cookieClick', cookieClick)
+    localStorage.setItem('boughtSunglasses', 'bought')
+
+    // save the bought item
+    localStorage.setItem('boughtItem', 'sunglasses-cookie')
+
+    // update the number of cookies left
+    document.getElementById('number-of-cookies').innerHTML = cookieClick + ' Cookies'
+
+    alert('You bought the Sunglasses Cookie!')
+  } else {
+    alert('Not enough cookies to buy this item.')
+  }
+}
+
+window.onload = function () {
+  // showing the number of cookies
+  const cookieClick = localStorage.getItem('cookieClick')
   document.getElementById('number-of-cookies').innerHTML = cookieClick + ' Cookies'
+
+  // update bought items images
+  const heartItemImage = document.getElementById('item-heart-image')
+  const starItemImage = document.getElementById('item-star-image')
+  const sunglassesItemImage = document.getElementById('item-sunglasses-image')
+
+  const heartItemBought = localStorage.getItem('boughtHeart')
+  const starItemBought = localStorage.getItem('boughtStar')
+  const sunglassesItemBought = localStorage.getItem('boughtSunglasses')
+
+  if (heartItemBought === 'bought') {
+    heartItemImage.src = './images/bought-item.png'
+  }
+  if (starItemBought === 'bought') {
+    starItemImage.src = './images/bought-item.png'
+  }
+  if (sunglassesItemBought === 'bought') {
+    sunglassesItemImage.src = './images/bought-item.png'
+  }
 }
---
+++ js/index.js
@@ -0,0 +1,47 @@
+// Copyright (c) 2025 Ain Jeong All rights reserved
+//
+// Created by: Ain Jeong
+// Created on: May 2025
+// This file contains the JS functions for cookie clicker
+
+'use strict'
+
+/**
+ * This function counts the number of clicking of cookie & all the functions happening in index.html
+ */
+// eslint-disable-next-line no-unused-vars
+function cookieClick () {
+  // input
+  let cookieClick = localStorage.getItem('cookieClick')
+
+  // process
+  if (cookieClick != null) {
+    cookieClick = Number(cookieClick) + 1
+  } else {
+    cookieClick = 1
+  }
+
+  // save in local storage
+  localStorage.setItem('cookieClick', cookieClick)
+
+  // output
+  document.getElementById('number-of-cookies').innerHTML = cookieClick + ' Cookies'
+}
+
+// wearing the bought items
+window.onload = function () {
+  const cookieImage = document.getElementById('cookie-image')
+  const heartBought = localStorage.getItem('boughtHeart')
+  const starBought = localStorage.getItem('boughtStar')
+  const sunglassesBought = localStorage.getItem('boughtSunglasses')
+
+  if (sunglassesBought === 'bought') {
+    cookieImage.src = './images/item-sunglasses.png'
+  } else if (starBought === 'bought') {
+    cookieImage.src = './images/item-star.png'
+  } else if (heartBought === 'bought') {
+    cookieImage.src = './images/item-heart.png'
+  } else {
+    cookieImage.src = './images/cookie.png'
+  }
+}
