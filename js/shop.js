1bb0ab80715ed6679ce76d1b83168b77e7d72cdc
Ain-Jeong <ain.jeong@stu.ocsb.ca>
2025-06-17 16:21:40 +0000

    edited js's

--- js/shop.js
+++ js/shop.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2025 Ain Jeong All rights reserved
 //
 // Created by: Ain Jeong
-// Created on: May 2025
+// Created on: June 2025
 // This file contains the JS functions for the shop
 
 'use strict'
